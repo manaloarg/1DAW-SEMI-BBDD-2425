032ca469147ed079067ac19a2c36ede985d5c6bc
Manuel Alonso Argente <145504941+manaloarg@users.noreply.github.com>
2025-02-12 07:45:24 +0100

    Update jardineria.sql

--- jardineria.sql
+++ jardineria.sql
@@ -2,107 +2,103 @@ DROP DATABASE IF EXISTS jardineria;
 CREATE DATABASE jardineria CHARACTER SET utf8mb4;
 USE jardineria;
 
-CREATE TABLE Oficinas (
-  CodigoOficina varchar(10) NOT NULL,
-  Ciudad varchar(30) NOT NULL,
-  Pais varchar(50) NOT NULL,
-  Region varchar(50) DEFAULT NULL,
-  CodigoPostal varchar(10) NOT NULL,
-  Telefono varchar(20) NOT NULL,
-  LineaDireccion1 varchar(50) NOT NULL,
-  LineaDireccion2 varchar(50) DEFAULT NULL,
-  PRIMARY KEY (CodigoOficina)
-) engine=innodb;
+CREATE TABLE oficina (
+  codigo_oficina VARCHAR(10),
+  ciudad VARCHAR(30) NOT NULL,
+  pais VARCHAR(50) NOT NULL,
+  region VARCHAR(50) DEFAULT NULL,
+  codigo_postal VARCHAR(10) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  linea_direccion1 VARCHAR(50) NOT NULL,
+  linea_direccion2 VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (codigo_oficina)
+);
 
-CREATE TABLE Empleados (
-  CodigoEmpleado integer NOT NULL,
-  Nombre varchar(50) NOT NULL,
-  Apellido1 varchar(50) NOT NULL,
-  Apellido2 varchar(50) DEFAULT NULL,
-  Extension varchar(10) NOT NULL,
-  Email varchar(100) NOT NULL,
-  CodigoOficina varchar(10) NOT NULL,
-  CodigoJefe integer DEFAULT NULL,
-  Puesto varchar(50) DEFAULT NULL,
-  PRIMARY KEY (CodigoEmpleado),
-  CONSTRAINT Empleados_OficinasFK FOREIGN KEY (CodigoOficina) REFERENCES Oficinas (CodigoOficina),
-  CONSTRAINT Empleados_EmpleadosFK FOREIGN KEY (CodigoJefe) REFERENCES Empleados (CodigoEmpleado)
-) engine=innodb;
+CREATE TABLE empleado (
+  codigo_empleado INT,
+  nombre VARCHAR(50) NOT NULL,
+  apellido1 VARCHAR(50) NOT NULL,
+  apellido2 VARCHAR(50) DEFAULT NULL,
+  extension VARCHAR(10) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  codigo_oficina VARCHAR(10) NOT NULL,
+  codigo_jefe INT DEFAULT NULL,
+  puesto VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (codigo_empleado),
+  FOREIGN KEY (codigo_oficina) REFERENCES oficina (codigo_oficina),
+  FOREIGN KEY (codigo_jefe) REFERENCES empleado (codigo_empleado)
+);
 
-CREATE TABLE GamasProductos (
-  Gama varchar(50) NOT NULL,
-  DescripcionTexto text,
-  DescripcionHTML text,
-  Imagen blob,
-  PRIMARY KEY (Gama)
-) engine=innodb;
+CREATE TABLE gama_producto (
+  gama VARCHAR(50),
+  descripcion_texto TEXT,
+  descripcion_html TEXT,
+  imagen VARCHAR(256),
+  PRIMARY KEY (gama)
+);
 
-CREATE TABLE Clientes (
-  CodigoCliente integer NOT NULL,
-  NombreCliente varchar(50) NOT NULL,
-  NombreContacto varchar(30) DEFAULT NULL,
-  ApellidoContacto varchar(30) DEFAULT NULL,
-  Telefono varchar(15) NOT NULL,
-  Fax varchar(15) NOT NULL,
-  LineaDireccion1 varchar(50) NOT NULL,
-  LineaDireccion2 varchar(50) DEFAULT NULL,
-  Ciudad varchar(50) NOT NULL,
-  Region varchar(50) DEFAULT NULL,
-  Pais varchar(50) DEFAULT NULL,
-  CodigoPostal varchar(10) DEFAULT NULL,
-  CodigoEmpleadoRepVentas integer DEFAULT NULL,
-  LimiteCredito numeric(15,2) DEFAULT NULL,
-  PRIMARY KEY (CodigoCliente),
-  CONSTRAINT Clientes_EmpleadosFK FOREIGN KEY (CodigoEmpleadoRepVentas) REFERENCES Empleados (CodigoEmpleado)
-) engine=innodb;
+CREATE TABLE cliente (
+  codigo_cliente INTEGER NOT NULL,
+  nombre_cliente VARCHAR(50) NOT NULL,
+  nombre_contacto VARCHAR(30) DEFAULT NULL,
+  apellido_contacto VARCHAR(30) DEFAULT NULL,
+  telefono VARCHAR(15) NOT NULL,
+  fax VARCHAR(15) NOT NULL,
+  linea_direccion1 VARCHAR(50) NOT NULL,
+  linea_direccion2 VARCHAR(50) DEFAULT NULL,
+  ciudad VARCHAR(50) NOT NULL,
+  region VARCHAR(50) DEFAULT NULL,
+  pais VARCHAR(50) DEFAULT NULL,
+  codigo_postal VARCHAR(10) DEFAULT NULL,
+  codigo_empleado_rep_ventas INTEGER DEFAULT NULL,
+  limite_credito NUMERIC(15,2) DEFAULT NULL,
+  PRIMARY KEY (codigo_cliente),
+  FOREIGN KEY (codigo_empleado_rep_ventas) REFERENCES empleado (codigo_empleado)
+);
 
-CREATE TABLE Pedidos (
-  CodigoPedido integer NOT NULL,
-  FechaPedido date NOT NULL,
-  FechaEsperada date NOT NULL,
-  FechaEntrega date DEFAULT NULL,
-  Estado varchar(15) NOT NULL,
-  Comentarios text,
-  CodigoCliente integer NOT NULL,
-  PRIMARY KEY (CodigoPedido),
-  CONSTRAINT Pedidos_Cliente FOREIGN KEY (CodigoCliente) REFERENCES Clientes (CodigoCliente)
-) engine=innodb;
+CREATE TABLE pedido (
+  codigo_pedido INTEGER NOT NULL,
+  fecha_pedido date NOT NULL,
+  fecha_esperada date NOT NULL,
+  fecha_entrega date DEFAULT NULL,
+  estado VARCHAR(15) NOT NULL,
+  comentarios TEXT,
+  codigo_cliente INTEGER NOT NULL,
+  PRIMARY KEY (codigo_pedido),
+  FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo_cliente)
+);
 
-CREATE TABLE Productos (
-  CodigoProducto varchar(15) NOT NULL,
-  Nombre varchar(70) NOT NULL,
-  Gama varchar(50) NOT NULL,
-  Dimensiones varchar(25) NULL,
-  Proveedor varchar(50) DEFAULT NULL,
-  Descripcion text NULL,
-  CantidadEnStock smallint NOT NULL,
-  PrecioVenta numeric(15,2) NOT NULL,
-  PrecioProveedor numeric(15,2) DEFAULT NULL,
-  PRIMARY KEY (CodigoProducto),
-  CONSTRAINT Productos_gamaFK FOREIGN KEY (Gama) REFERENCES GamasProductos (Gama)
-) engine=innodb;
+CREATE TABLE producto (
+  codigo_producto VARCHAR(15) NOT NULL,
+  nombre VARCHAR(70) NOT NULL,
+  gama VARCHAR(50) NOT NULL,
+  dimensiones VARCHAR(25) NULL,
+  proveedor VARCHAR(50) DEFAULT NULL,
+  descripcion text NULL,
+  cantidad_en_stock SMALLINT NOT NULL,
+  precio_venta NUMERIC(15,2) NOT NULL,
+  precio_proveedor NUMERIC(15,2) DEFAULT NULL,
+  PRIMARY KEY (codigo_producto),
+  FOREIGN KEY (gama) REFERENCES gama_producto (gama)
+);
 
-CREATE TABLE DetallePedidos (
-  CodigoPedido integer NOT NULL,
-  CodigoProducto varchar(15) NOT NULL,
-  Cantidad integer NOT NULL,
-  PrecioUnidad numeric(15,2) NOT NULL,
-  NumeroLinea smallint NOT NULL,
-  PRIMARY KEY (CodigoPedido,CodigoProducto),
-  CONSTRAINT DetallePedidos_PedidoFK FOREIGN KEY (CodigoPedido) REFERENCES Pedidos (CodigoPedido),
-  CONSTRAINT DetallePedidos_ProductoFK FOREIGN KEY (CodigoProducto) REFERENCES Productos (CodigoProducto)
-)engine=innodb;
+CREATE TABLE detalle_pedido (
+  codigo_pedido INTEGER NOT NULL,
+  codigo_producto VARCHAR(15) NOT NULL,
+  cantidad INTEGER NOT NULL,
+  precio_unidad NUMERIC(15,2) NOT NULL,
+  numero_linea SMALLINT NOT NULL,
+  PRIMARY KEY (codigo_pedido, codigo_producto),
+  FOREIGN KEY (codigo_pedido) REFERENCES pedido (codigo_pedido),
+  FOREIGN KEY (codigo_producto) REFERENCES producto (codigo_producto)
+);
 
-CREATE TABLE Pagos (
-  CodigoCliente integer NOT NULL,
-  FormaPago varchar(40) NOT NULL,
-  IDTransaccion varchar(50) NOT NULL,
-  FechaPago date NOT NULL,
-  Cantidad numeric(15,2) NOT NULL,
-  PRIMARY KEY (CodigoCliente,IDTransaccion),
-  CONSTRAINT Pagos_clienteFK FOREIGN KEY (CodigoCliente) REFERENCES Clientes (CodigoCliente)
-) engine=innodb;
-
-
-
-
+CREATE TABLE pago (
+  codigo_cliente INTEGER NOT NULL,
+  forma_pago VARCHAR(40) NOT NULL,
+  id_transaccion VARCHAR(50) NOT NULL,
+  fecha_pago date NOT NULL,
+  total NUMERIC(15,2) NOT NULL,
+  PRIMARY KEY (codigo_cliente, id_transaccion),
+  FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo_cliente)
+);
